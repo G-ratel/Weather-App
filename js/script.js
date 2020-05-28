e63ba23f1fb26bd7c4f8301984a9b842bf1e856f
Grace Ajiboye <60976458+G-ratel@users.noreply.github.com>
2020-05-28 22:25:34 +0100

    Weather-App

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let button = document.querySelector('.btn');
+let inputVal = document.querySelector('.inputVal');
+let name = document.querySelector('.name');
+let desc = document.querySelector('.desc');
+let wind = document.querySelector('.wind');
+let temp = document.querySelector('.temp');
+
+button.addEventListener('click',function(){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputVal.value+'&appid=4030ac742b531a108804ed9c3c7be4ba')
+    .then(response => response.json())
+    // .then(data => console.log(data))
+    .then(data => {
+        let nameValue = data['name'];
+        let tempValue = data['main']['temp'];
+        let windValue = data['wind']['speed'];
+        let descValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        wind.innerHTML = windValue;
+        desc.innerHTML = descValue;
+    })
+.catch(err => alert("Wrong city name!"))
+})
