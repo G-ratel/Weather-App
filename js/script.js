65e1f4995f8b44528e188d929d26f8ce92187684
Grace Ajiboye <60976458+G-ratel@users.noreply.github.com>
2020-05-30 20:36:12 +0100

    Weather-App

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let wind = document.querySelector('.wind');
 let temp = document.querySelector('.temp');
 
 button.addEventListener('click',function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputVal.value+'&appid=4030ac742b531a108804ed9c3c7be4ba')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputVal.value+'&appid=4030ac742b531a108804ed9c3c7be4ba')
     .then(response => response.json())
     // .then(data => console.log(data))
     .then(data => {
